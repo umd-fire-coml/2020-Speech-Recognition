b6bafa07b82df8600c7f3ac0656b87f2df209450
Rohan Angajala <rohankms@gmail.com>
2020-09-19 18:40:38 -0700

    Wrote Data checker for folder/file argument

---
+++ checker.py
@@ -0,0 +1,13 @@
+import argparse, os
+from argparse import ArgumentParser
+
+parser = argparse.ArgumentParser(description = 'Dataset Checker')
+parser.add_argument('folder', type = str, help = 'Folder to Check for')
+args = parser.parse_args()
+
+folder = args.folder
+
+if (os.path.exists(args.folder)):
+    print("Data exists on the current path.")
+else:
+    print("Data does not exist on the current path.")
