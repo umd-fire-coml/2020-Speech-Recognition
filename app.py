b7bcc54659fae8133de5e434b2a8c6905bf66353
ranga2000 <70935530+ranga2000@users.noreply.github.com>
2020-11-17 12:35:25 -0800

    Updated model2 version

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from io import BufferedReader
-import visualization
+import baseline_model, model2
 
 app = Flask(__name__)
 
@@ -13,11 +13,8 @@ def upload_file():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         uploaded_file.save(uploaded_file.filename)
-    #uploaded_file = BufferedReader(uploaded_file.stream)
-    #print(type(uploaded_file.filename))
-    res = visualization.visualizer(uploaded_file.filename)
-    #return str(res)
-    return render_template('index.html', data=res)
+    res3 = model2.parse_audio(uploaded_file.filename)
+    return render_template('index.html', data=res3)
   
 
 if __name__ == "__main__":
